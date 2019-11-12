7e4b8b311b41f7e5c7802ce8cc24b8fe11873e32
Abdullah Alani <alani@Abdullahs-MacBook-Air.local>
2019-11-12 13:28:29 -0600

    Updates

---
+++ precache-manifest.6b3fef006acc5cb057aeb04a0cf7bc6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8ad196a00263a63e6efcae2d1efefe9",
+    "url": "/alani989/zoovu-challenge.git/index.html"
+  },
+  {
+    "revision": "6e70423c46f758dcf1ea",
+    "url": "/alani989/zoovu-challenge.git/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "4fa221503d41d49c2924",
+    "url": "/alani989/zoovu-challenge.git/static/css/main.d06e755f.chunk.css"
+  },
+  {
+    "revision": "6e70423c46f758dcf1ea",
+    "url": "/alani989/zoovu-challenge.git/static/js/2.a0d34a8e.chunk.js"
+  },
+  {
+    "revision": "4fa221503d41d49c2924",
+    "url": "/alani989/zoovu-challenge.git/static/js/main.606bfadc.chunk.js"
+  },
+  {
+    "revision": "a6cfe069bed1b14fcc48",
+    "url": "/alani989/zoovu-challenge.git/static/js/runtime-main.bc97ef4b.js"
+  }
+]);
